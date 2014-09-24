95ae4d1c86e77dff0c8aea78c074efc8986ad5b1
xiaolin99 <xiao.lin99@gmail.com>
2014-09-24 11:46:52 -0600

    Part1 WIP

--- src/HTTrack.java
+++ src/HTTrack.java
@@ -15,9 +15,7 @@ public class HTTrack {
 	
 		try
 		{
-			File downloadFile = new File("./"+host+path);
-			downloadFile.getParentFile().mkdirs();
-			PrintWriter fileWriter = new PrintWriter(downloadFile);
+			
 			
 			Socket socket = new Socket(host, port);
 			PrintWriter request = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
@@ -27,20 +25,53 @@ public class HTTrack {
 			
 			BufferedReader download = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			String line = download.readLine();
+			if (!line.contains("200")) {
+				// expect return code 200 OK
+				download.close();
+				socket.close();
+				return;
+			}
+			
+			// skip the header
 			while (line != null) {
-				System.out.println(line);
-				fileWriter.println(line);
+				line = download.readLine();
+				if (line.length() == 0) break;
+			}
+			
+			File downloadFile = new File("./"+host+path);
+			downloadFile.getParentFile().mkdirs();
+			PrintWriter fileWriter = new PrintWriter(downloadFile);
+			int charI = download.read();
+			while (charI != -1) {
+				// cannot write header to file
+
+
+				System.out.print(Character.toChars(charI));
+				fileWriter.print(Character.toChars(charI));
+
 				if (line.toLowerCase().contains("<a href=")) {
 					// recursively download more URL
 					String refLink = "";
 					try {
 						URL refURL = new URL(refLink);
+						// ref includes hostname, so check if it's the same host
+						String refHost = refURL.getHost();
+							if (refHost.equalsIgnoreCase(host)) {
+								String refPath = refURL.getPath();
+								int refPort = refURL.getPort();
+								if (refPort == -1) refPort = refURL.getDefaultPort();
+								if (refPort == -1) refPort = 80;
+								downloadFromURL(refHost, refPath, refPort);
+						}
+						
 					}
 					catch (MalformedURLException e) {
+						// ref is not a URL, so it's a relative link
 						
 					}
 				}
-				line = download.readLine();
+				
+				charI = download.read();
 			}
 			
 			download.close();

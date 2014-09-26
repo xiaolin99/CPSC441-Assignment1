73fe7318cc8df9ba6dae03c23e6a2603a8c6a442
xiaolin99 <xiao.lin99@gmail.com>
2014-09-26 11:45:45 -0600

    Part3 WIP

---
+++ src/WebSync.java
@@ -0,0 +1,21 @@
+/**
+ * CPSC 441 Assignment 1 Part3
+ * @author Xiao Lin
+ */
+import java.net.*;
+import java.io.*;
+
+public class WebSync {
+
+	private static void sync(URL aURL) {
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
